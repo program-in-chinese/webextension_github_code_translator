a4475400018126d7af411a95eb4b6ff391a80d44
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-10-16 15:42:43 -0700

    调用英汉词典, 代替原本的命名词典; 取代码所用的编程语言

--- 弹窗/主界面.js
+++ 弹窗/主界面.js
@@ -1,19 +1,43 @@
-/* 大小写敏感
+/* 尽量仅对命名进行翻译, 中间带空格的部分不翻译
 */
 
 var 关键词词典 = {};
 
+function 添加所有待查词(字段列表) {
+  var 单词 = 取所有单词(字段列表);
+  for (var i in 单词) {
+    命名词典[单词[i]] = false;
+  }
+}
+
+function 取编程语言(顶节点) {
+  var 节点类型 = 顶节点.classList;
+  if (节点类型.length == 3 && 节点类型[2].indexOf('type-') == 0) {
+    return 节点类型[2].substring(5);
+  }
+}
+
 function 翻译() {
+  var 原代码拷贝 = document.getElementsByTagName('table')[0];
+  var 顶节点 = 原代码拷贝.parentElement;
+  var 编程语言 = 取编程语言(顶节点);
+  var span字段列表 = 原代码拷贝.getElementsByTagName('span');
+  var 文本字段列表 = 取子文本节点(document);
+
+  // 合并两个部分
+  //添加所有待查词(span字段列表);
+  添加所有待查词(文本字段列表);
+
   chrome.runtime.sendMessage(
-    "koeedjcjmjpnahckonloagpkhankpneh",
+    "ndifefelacmidghjaehmhicbchbidhpe",
     命名词典,
     function(返回值) {
       命名词典 = 返回值.所有释义;
-      var 原代码拷贝 = document.getElementsByTagName('table')[0];
-      var span字段列表 = 原代码拷贝.getElementsByTagName('span');
+      console.log(命名词典);
       翻译字段列表(span字段列表);
-      var 文本字段列表 = 取子文本节点(document);
       翻译字段列表(文本字段列表);
+
+      顶节点.insertBefore(document.createTextNode(编程语言), 原代码拷贝);
     }
   );
 }
@@ -25,12 +49,19 @@ function 取子文本节点(el) {
 }
 
 // 假设每个字段除了词, 其他都是非英文字符.
+// 仅翻译无空格的片段
 function 取字段中所有词(字段) {
-  var 单词 = 字段.match(/[a-zA-Z]+/g);
+  var 删除前后空格 = 字段.textContent.trim();
+  // 确认无空格
+  if (!删除前后空格.match(/^[^\s]+$/g)) {
+    return [];
+  }
+  var 单词 = 删除前后空格.match(/[a-zA-Z]+/g);
   if (单词) {
+    //console.log(删除前后空格);
     return 单词;
   }
-  return [字段];
+  return [];
 }
 
 function 取字段中最长句(字段) {
@@ -41,41 +72,46 @@ function 取字段中最长句(字段) {
   return 字段;
 }
 
+// 删去所有前后空格后再提取单词
+function 取所有单词(字段列表) {
+  var 所有单词 = [];
+  for (var i = 0; i < 字段列表.length; i++) {
+    var 字段中单词 = 取字段中所有词(字段列表[i]);
+    所有单词 = 所有单词.concat(字段中单词);
+  }
+  return 所有单词;
+}
+
 function 翻译字段列表(字段列表) {
   for (var i = 0; i < 字段列表.length; i++) {
     var 字段 = 字段列表[i].textContent;
-    var 所有单词 = 取字段中所有词(字段);
-    var 所有单词有翻译 = false;
+
+    // TODO: 避免重复分析字段
+    var 所有单词 = 取字段中所有词(字段列表[i]);
+    //var 所有单词有翻译 = false;
     for (var j = 0; j < 所有单词.length; j++) {
       var 单词 = 所有单词[j];
       var 对应中文词 = 关键词词典[单词] || API词典[单词] || 命名词典[单词];
       if (对应中文词) {
-        if (j == 所有单词.length - 1) {
+        /*if (j == 所有单词.length - 1) {
           所有单词有翻译 = true;
-        }
-      } else {
+        }*/
+        字段 = 字段.replace(单词, 对应中文词);
+      }/* else {
         break;
-      }
-      字段 = 字段.replace(单词, 对应中文词);
+      }*/
     }
-    // 取巧: 仅当字段中所有词有翻译时才替换字段, 避免某些文本中出现个别可识别的单词. 今后需进行语法分析.
-    if (所有单词有翻译) {
-      字段列表[i].textContent = 字段;
-    } else {
-      var 句 = 取字段中最长句(字段);
-      var 对应中文 = 语句翻译[句.toLowerCase()]
-      if (对应中文) {
-        字段列表[i].textContent = 字段.replace(句, 对应中文);
-      }
-    }
+    // TODO: 避免某些文本中出现个别可识别的单词. 今后需进行语法分析.
+    字段列表[i].textContent = 字段;
   }
 }
 
 function 获取代码段() {
-  //You can play with your DOM here or check URL against your regex
-  var 代码段节点 = document.body.getElementsByTagName('table')[0].outerHTML
+  var 代码段节点 = document.body.getElementsByTagName('table')[0]
+  // 父节点的class包含编程语言信息, 如class="blob-wrapper data type-python "
+  var 代码段 = 代码段节点.parentElement.outerHTML
   //console.log(代码段节点);
-  return [代码段节点];
+  return [代码段];
 }
 
 // 需允许访问activeTab, 才能调用chrome.tabs.executeScript:
--- 弹窗/词典.js
+++ 弹窗/词典.js
@@ -7,18 +7,18 @@ var API词典 = {
 }
 
 var 命名词典 = {
-  'shoplist': '购物单',
+  /*'shoplist': '购物单',
   'apple': '苹果',
   'mango': '芒果',
   'carrot': '胡萝卜',
   'banana': '香蕉',
   'rice': '米',
   'item': '物品',
-  'olditem': '第一项'
+  'olditem': '第一项'*/
 }
 
 // 这里使用有道在线翻译结果. TODO: 用翻译API代替
-var 语句翻译 = {
+/*var 语句翻译 = {
   'this is my shopping list': '这是我的购物单',
   'i have': '我有',
   'items to purchase': '要购买的产品',
@@ -27,4 +27,4 @@ var 语句翻译 = {
   'i will sort my list now': '我现在就整理我的清单',
   'sorted shopping list is': '排序的购物清单是',
   'the first item i will buy is': '我要买的第一件东西是'
-}
+}*/
