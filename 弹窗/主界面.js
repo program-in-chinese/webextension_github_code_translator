d2b67f3afd22416c710414204b5dac56e63bc957
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-10-15 23:19:03 -0700

    #1 调用英汉词典批量查词接口, 需确认ID. 拆分词典常量

--- 弹窗/主界面.js
+++ 弹窗/主界面.js
@@ -3,43 +3,19 @@
 
 var 关键词词典 = {};
 
-var API词典 = {
-  'print': '打印',
-  'append': '添加',
-  'sort': '排序',
-  'len': '长度',
-  'end': '结尾'
-}
-
-var 命名词典 = {
-  'shoplist': '购物单',
-  'apple': '苹果',
-  'mango': '芒果',
-  'carrot': '胡萝卜',
-  'banana': '香蕉',
-  'rice': '米',
-  'item': '物品',
-  'olditem': '第一项'
-}
-
-// 这里使用有道在线翻译结果. TODO: 用翻译API代替
-var 语句翻译 = {
-  'this is my shopping list': '这是我的购物单',
-  'i have': '我有',
-  'items to purchase': '要购买的产品',
-  'i also have to buy rice': '我还得买大米',
-  'my shopping list is now': '我的购物单现在在',
-  'i will sort my list now': '我现在就整理我的清单',
-  'sorted shopping list is': '排序的购物清单是',
-  'the first item i will buy is': '我要买的第一件东西是'
-}
-
 function 翻译() {
-  var 原代码拷贝 = document.getElementsByTagName('table')[0];
-  var span字段列表 = 原代码拷贝.getElementsByTagName('span');
-  翻译字段列表(span字段列表);
-  var 文本字段列表 = 取子文本节点(document);
-  翻译字段列表(文本字段列表);
+  chrome.runtime.sendMessage(
+    "koeedjcjmjpnahckonloagpkhankpneh",
+    命名词典,
+    function(返回值) {
+      命名词典 = 返回值.所有释义;
+      var 原代码拷贝 = document.getElementsByTagName('table')[0];
+      var span字段列表 = 原代码拷贝.getElementsByTagName('span');
+      翻译字段列表(span字段列表);
+      var 文本字段列表 = 取子文本节点(document);
+      翻译字段列表(文本字段列表);
+    }
+  );
 }
 
 function 取子文本节点(el) {
@@ -98,7 +74,7 @@ function 翻译字段列表(字段列表) {
 function 获取代码段() {
   //You can play with your DOM here or check URL against your regex
   var 代码段节点 = document.body.getElementsByTagName('table')[0].outerHTML
-  console.log(代码段节点);
+  //console.log(代码段节点);
   return [代码段节点];
 }
 
---
+++ 弹窗/词典.js
@@ -0,0 +1,30 @@
+var API词典 = {
+  'print': '打印',
+  'append': '添加',
+  'sort': '排序',
+  'len': '长度',
+  'end': '结尾'
+}
+
+var 命名词典 = {
+  'shoplist': '购物单',
+  'apple': '苹果',
+  'mango': '芒果',
+  'carrot': '胡萝卜',
+  'banana': '香蕉',
+  'rice': '米',
+  'item': '物品',
+  'olditem': '第一项'
+}
+
+// 这里使用有道在线翻译结果. TODO: 用翻译API代替
+var 语句翻译 = {
+  'this is my shopping list': '这是我的购物单',
+  'i have': '我有',
+  'items to purchase': '要购买的产品',
+  'i also have to buy rice': '我还得买大米',
+  'my shopping list is now': '我的购物单现在在',
+  'i will sort my list now': '我现在就整理我的清单',
+  'sorted shopping list is': '排序的购物清单是',
+  'the first item i will buy is': '我要买的第一件东西是'
+}
