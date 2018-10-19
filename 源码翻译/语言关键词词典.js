2c15611e00f0ca559b7a2ba8d9c7ea65b6498b00
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-10-19 12:45:05 -0700

    添加JS关键词

--- 源码翻译/语言关键词词典.js
+++ 源码翻译/语言关键词词典.js
@@ -1,3 +1,4 @@
+// TODO: 并非所有编程语言都有. 会影响命名翻译.
 var 通用关键词 = {
   'assert': "断言",
   'import': "导入",
@@ -18,6 +19,58 @@ var 通用关键词 = {
 }
 
 var 专用关键词 = {
+  "javascript": {
+    "abstract": "抽象",
+    "arguments": "参数",
+    "await": "等待",
+    "boolean": "布尔",
+    "byte": "字节",
+    "catch": "接住",
+    "char": "字节",
+    "const": "const",
+    "debugger": "调试器",
+    "default": "默认",
+    "delete": "删除",
+    "do": "执行",
+    "double": "双浮点",
+    "enum": "枚举",
+    "eval": "eval",
+    "export": "导出",
+    "extends": "扩展",
+    "false": "假",
+    "final": "最终",
+    "float": "单浮点",
+    "function": "函数",
+    "goto": "goto",
+    "implements": "实现",
+    'instanceof': "为实例",
+    'int': "整数",
+    'interface': "接口",
+    "let": "让",
+    "long": "长整数",
+    'native': "native",
+    'new': "新",
+    "null": "空",
+    'package': "包",
+    'private': "私有",
+    'protected': "保护",
+    'public': "公有",
+    'short': "短整数",
+    'static': "静态",
+    'super': "父类",
+    'synchronized': "同步",
+    'this': "本身",
+    'throws': "会抛出",
+    'transient': "transient",
+    'true': "真",
+    "try": "尝试",
+    "typeof": "为类型",
+    "var": "变量",
+    "void": "无值",
+    "volatile": "volatile",
+    "with": "with",
+    "yield": "产出"
+  },
   /*
   参考 http://zetcode.com/lang/python/keywords/
   import keyword
@@ -49,6 +102,7 @@ var 专用关键词 = {
     'abstract': "抽象",
     'boolean': "布尔",
     'byte': "字节",
+    'catch': "接住",
     'char': "字符",
     'const': "const",
     'default': "默认",
@@ -56,6 +110,7 @@ var 专用关键词 = {
     'double': "双浮点",
     'enum': "枚举",
     'extends': "扩展",
+    'false': "假",
     'final': "最终",
     'float': "单浮点",
     'goto': "goto",
@@ -66,6 +121,7 @@ var 专用关键词 = {
     'long': "长整数",
     'native': "native",
     'new': "新",
+    'null': "空",
     'package': "包",
     'private': "私有",
     'protected': "保护",
@@ -78,9 +134,90 @@ var 专用关键词 = {
     'this': "本身",
     'throws': "会抛出",
     'transient': "transient",
+    'true': "真",
+    'try': "尝试",
     'void': "无值",
     'volatile': "volatile"
   }
+}
+
+const JavaScript关键词 = [
+"abstract",
+"break",
+"char",
+"debugger",
+"double",
+"export",
+"finally",
+"goto",
+"in",
+"let",
+"null",
+"public",
+"super",
+"throw",
+"try",
+"volatile",
+"arguments",
+"byte",
+"class",
+"default",
+"else",
+"extends",
+"float",
+"if",
+"instanceof",
+"long",
+"package",
+"return",
+"switch",
+"throws",
+"typeof",
+"while",
+"await",
+"case",
+"const",
+"delete",
+"enum",
+"false",
+"for",
+"implements",
+"int",
+"native",
+"private",
+"short",
+"synchronized",
+"transient",
+"var",
+"with",
+"boolean",
+"catch",
+"continue",
+"do",
+"eval",
+"final",
+"function",
+"import",
+"interface",
+"new",
+"protected",
+"static",
+"this",
+"true",
+"void",
+"yield"
+];
+
+// 仅用于控制台下运行, 将通用关键词剔除后, 再加入专用关键词词典
+function 取专有关键词(所有关键词) {
+  var 非通用关键词 = [];
+  所有关键词.sort();
+  for (词 of 所有关键词) {
+    if (!(词 in 通用关键词)) {
+      非通用关键词.push(词);
+    }
+  }
+  console.log(非通用关键词);
 }
 
 function 取所有关键词(编程语言) {
